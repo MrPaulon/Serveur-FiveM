b614b82164027087e3bd82cd73a74d6cbe85103f
MrPaulon <pro.paulon@gmail.com>
2021-04-07 19:50:28 +0200

    Fichiers de base

--- mysql-async/src/webpack.config.js
+++ mysql-async/src/webpack.config.js
@@ -1,46 +1,14 @@
 const path = require('path');
 
-const baseConfig = {
-  mode: 'production',
+module.exports = {
+  entry: './mysql.js',
   target: 'node',
-  optimization: {
-    minimize: false,
-  },
-  resolve: {
-    extensions: [ '.ts', '.js' ],
-  },
-  module: {
-    rules: [
-      {
-        test: /.ts$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  targets: {
-                    node: true,
-                  },
-                },
-              ],
-            ],
-            plugins: ["@babel/plugin-transform-typescript"],
-          },
-        },
-      },
-    ],
-  },
-};
-
-const serverConfig = {
-  entry: './entry/server.ts',
+  mode: 'production',
   output: {
     filename: 'mysql-async.js',
     path: path.resolve(__dirname, '..'),
   },
-  ...baseConfig,
+  optimization: {
+    minimize: false,
+  },
 };
-
-module.exports = serverConfig;
